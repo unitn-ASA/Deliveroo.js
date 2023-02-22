b9d7e079a04d58a9d4e4c76f8b3f90d5ab5f5527
Marco Robol <marco.robol@unitn.it>
2023-02-22 16:00:53 +0100

    no parcels on borders

--- my_game.js
+++ my_game.js
@@ -37,8 +37,8 @@ const myGrid = new Grid(myMap);
 
 
 myClock.on( '2s', () => {
-    let x = Math.floor(Math.random()*10);
-    let y = Math.floor(Math.random()*10);
+    let x = Math.floor(Math.random()*8) + 1;
+    let y = Math.floor(Math.random()*8) + 1;
     let parcel = myGrid.createParcel(x, y);
     if (parcel)
         console.log('parcel created at', x, y, parcel.reward)
