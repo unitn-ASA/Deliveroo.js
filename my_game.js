c1dc7344f735399faa97077eceb73e5c6b98a022
Marco Robol <marco.robol@unitn.it>
2023-03-24 10:36:51 +0100

    not logging parcel created msg

--- my_game.js
+++ my_game.js
@@ -40,8 +40,8 @@ myClock.on( '2s', () => {
     let x = Math.floor(Math.random()*8) + 1;
     let y = Math.floor(Math.random()*8) + 1;
     let parcel = myGrid.createParcel(x, y);
-    if (parcel)
-        console.log('parcel created at', x, y, parcel.reward)
+    // if (parcel)
+    //     console.log('parcel created at', x, y, parcel.reward)
 } )
 
 
