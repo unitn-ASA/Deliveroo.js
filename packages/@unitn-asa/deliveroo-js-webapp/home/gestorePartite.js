bdc1d3aaec8c20cf87405e17f548902f2b4229fd
matteogrisenti <matteo.grisenti@studenti.unitn.it>
2023-12-07 10:27:07 +0100

    Inserimento Parametro game_number
    Quando l'utente clicca il bottone partecipa partita n, viene inserito nell'URL il parametro game_number inizializzato con il numero della partita definito sul bottone

--- packages/@unitn-asa/deliveroo-js-webapp/home/gestorePartite.js
+++ packages/@unitn-asa/deliveroo-js-webapp/home/gestorePartite.js
@@ -1,7 +1,16 @@
-const partecipaBtn = document.getElementById('partecipaBtn');
+const partecipaBtn = document.getElementsByClassName('partecipaBtn');
 
-partecipaBtn.addEventListener('click', function() {
+Array.from(partecipaBtn).forEach(bottone => {
+    bottone.addEventListener('click', function() {
+      
+        console.log(bottone.getAttribute('game'));
 
-    window.location.href = 'game';
-    
+        var game_number = bottone.getAttribute('game');
+
+        var url = 'game';
+        url += '?game_number=' + encodeURIComponent(game_number); 
+
+        window.location.href = url; 
+      
+    });
 });
