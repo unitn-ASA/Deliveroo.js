92bf1ba06bdc1bd53d2bc9d107e133d1ef1e9bfb
Marco Robol <marco.robol@unitn.it>
2025-05-06 00:36:45 +0200

    exportMap() button

--- frontend/src/components/modals/Maps.vue
+++ frontend/src/components/modals/Maps.vue
@@ -21,12 +21,52 @@
         emit('loadMap', map );
     }
 
+    function exportMap() {
+        const grid = connection.grid;
+        const WIDTH = Array.from(grid.tiles.values()).reduce( (max, tile) => Math.max(max, tile.x), 0 ) + 1;
+        const HEIGHT = Array.from(grid.tiles.values()).reduce( (max, tile) => Math.max(max, tile.y), 0 ) + 1;
+        // const WIDTH = grid.width;
+        // const HEIGHT = grid.height;
+        const tiles = grid.tiles;
+        const map = [];
+        for ( let x=0; x<WIDTH; x++ ) {
+            const row = []
+            for ( let y=0; y<HEIGHT; y++ ) {
+                if ( tiles.has(x + y*1000) ) {
+                    let tile = tiles.get( x + y*1000 );
+                    row.push(tile.type);
+                }
+                else {
+                    row.push(0);
+                }
+            }
+            map.push(row);
+        }
+        var string = "[\n";
+        string += map.map( row => '\t[' + row.join(', ') + ']' ).join( ',\n' )
+        // for (const row of map) {
+        //     string += '\t[' + row.join(', ') + '],\n';
+        // }
+        string += "\n]";
+        console.log( string );
+        // copy into clipboard
+        navigator.clipboard.writeText( string );
+        alert( "Map copied to clipboard!" );
+        return map;
+    }
+
 </script>
 
 <template>
     <main>
 
     <div class="w-6/8 mx-auto pb-10 space-y-4">
+        
+        <div class="flex flex-wrap justify-center p-2">
+            <button class="btn btn-info btn-sm" @click="exportMap()">
+                Export Map
+            </button>
+        </div>
         
         <div class="flex flex-wrap justify-center">
 
